a3422bd13b248b47c882bb26268c426dcddde9c7
nordt-ryan <nordt.ryan@gmail.com>
2026-08-29 13:53:26 -0700

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def in_autotests_we_trust(a, b):
-    if a == b:
-        print('PASSED')
-    else:
-        print('FAILED')
-
-in_autotests_we_trust(10, '10')
-
-in_autotests_we_trust(0, False)
+print("user 1")
